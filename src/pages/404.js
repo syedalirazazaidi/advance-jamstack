b3e1e3e8d7d0ecb309922ee9c3f9329b0d1defb9
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-04-08 12:55:15 +0500

    final

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,7 @@
 import React from "react"
 
 const Ermsg = () => {
-  return (
-    <div>
-      <h1>404 Error Found !</h1>
-    </div>
-  )
+  return <div>{/* <h1>404 Error Found !</h1> */}</div>
 }
 
 export default Ermsg
